7e91b5ea833bb5a1120ba2a978b1e4648f674972
kk <conleylee@foxmail.com>
2025-12-04 09:33:29 +0800

    new tensor

--- src/vkllm_tensor.h
+++ src/vkllm_tensor.h
@@ -6,18 +6,20 @@
 #include "vkllm_common.h"
 #include "vkllm_context.h"
 #include "vkllm_dtypes.h"
+#include "vkllm_gpu_device.h"
 #include "vkllm_ops.h"
 #include <stdbool.h>
 
 struct vkllm_tensor
 {
-    char *name;
+    const char *name;
     void *host_buf;
     vkllm_dtype_t dtype;
     uint32_t shapes[4];
     uint32_t strides[4];
     uint32_t bytes;
 
+    struct vkllm_gpu_device *device;
     struct
     {
         VmaAllocation allocation;
@@ -31,7 +33,7 @@ struct vkllm_tensor
     bool mapped; // host_buf is mapped
 };
 
-extern vkllm_err_t vkllm_new_tensor(struct vkllm_context *context, const uint32_t *shapes, const uint32_t n_shape,
-                                    vkllm_dtype_t dtype, struct vkllm_tensor *srcs, const uint32_t n_srcs, void *params,
-                                    bool mapped, struct vkllm_tensor **p);
+extern vkllm_err_t vkllm_new_tensor(struct vkllm_context *context, const char *name, const uint32_t *shapes,
+                                    vkllm_dtype_t dtype, struct vkllm_tensor **srcs, const uint32_t n_srcs,
+                                    void *params, bool mapped, struct vkllm_tensor **p);
 #endif
