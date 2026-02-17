cf1594ecf119413a2525c0b7b7b806ca32ee4de0
KK <conleylee@foxmail.com>
2026-02-17 14:27:54 +0800

    new tensor

--- src/vkllm_dtypes.h
+++ src/vkllm_dtypes.h
@@ -9,24 +9,40 @@ typedef enum
 #undef _VKLLM_DTYPE_OP
 } vkllm_dtype_t;
 
-extern const char *vkllm_dtype_s(vkllm_dtype_t dtype);
-inline uint32_t vkllm_dtype_size(vkllm_dtype_t dtype)
+struct vkllm_dtype_info_t
 {
+    vkllm_dtype_t dtype;
+    uint32_t bytes;
+    uint32_t items_per_block;
+    uint32_t bytes_per_block;
+};
+
+extern const char *vkllm_dtype_s(vkllm_dtype_t dtype);
+inline struct vkllm_dtype_info_t vkllm_dtype_bytes(vkllm_dtype_t dtype)
+{
+    struct vkllm_dtype_info_t info;
+    info.dtype = dtype;
     if (dtype == vkllm_float32)
     {
-        return sizeof(float);
+        info.bytes = sizeof(float);
+        info.items_per_block = 1;
+        info.bytes_per_block = info.bytes * info.items_per_block;
     }
     else if (dtype == vkllm_float16)
     {
-        return sizeof(uint16_t);
+        info.bytes = sizeof(uint16_t);
+        info.items_per_block = 1;
+        info.bytes_per_block = info.bytes * info.items_per_block;
     }
     else if (dtype == vkllm_int8)
     {
-        return sizeof(int8_t);
-    }
-    else
-    {
-        return 0;
-    }
+        info.bytes = sizeof(int8_t);
+        info.items_per_block = 1;
+        info.bytes_per_block = info.bytes * info.items_per_block;
+    }else
+       {
+       }
+
+    return info;
 }
 #endif
