35db5ebbfe0379ec245fda40402d90027ef66ff6
KK <conleylee@foxmail.com>
2026-02-17 14:27:53 +0800

    debug: segment fault

--- tests/test_new_gpu.c
+++ tests/test_new_gpu.c
@@ -8,24 +8,25 @@
 
 int main(int argc, const char* argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "usage ./%s <gpu id>\n", argv[0]);
-        return -1;
+	fprintf(stderr, "usage ./%s <gpu id>\n", argv[0]);
+	return -1;
     }
 
     struct vkllm_context* context = NULL;
     vkllm_err_t e = vkllm_new_context(&context);
     if (e != VKLLM_ERR_OK) {
-        log_error("vkllm_new_context failed: %s", vkllm_err_s(e));
-        return -1;
+	log_error("vkllm_new_context failed: %s", vkllm_err_s(e));
+	return -1;
     }
 
     int id = atoi(argv[1]);
     struct vkllm_gpu_device* gpu = NULL;
     e = vkllm_new_gpu_device(context, id, &gpu);
     if (e != VKLLM_ERR_OK) {
-        log_error("vkllm_new_gpu_device failed: %s", vkllm_err_s(e));
-        return -1;
+	log_error("vkllm_new_gpu_device failed: %s", vkllm_err_s(e));
+	return -1;
     }
 
+    vkllm_destroy_gpu_device(context, gpu);
     return 0;
 }
