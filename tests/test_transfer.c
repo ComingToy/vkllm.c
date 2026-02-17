afe0c5f0451a62c6c0f166f95fffc92919ad8383
KK <conleylee@foxmail.com>
2026-02-17 14:27:54 +0800

    fix gpu free

--- tests/test_transfer.c
+++ tests/test_transfer.c
@@ -1,7 +1,29 @@
+#include "log.h"
 #include "src/vkllm_commands.h"
+#include "src/vkllm_context.h"
 #include "src/vkllm_gpu_device.h"
 #include "src/vkllm_tensor.h"
 #include <stdio.h>
+
+void random_buf(float *a, const size_t n)
+{
+    for (size_t i = 0; i < n; ++i)
+    {
+        a[i] = 10.0 * (rand() % 100) / 100.0;
+    }
+}
+
+float float_mse(const float *a, const float *b, const size_t n)
+{
+    float err = .0f;
+    float w = 1.0f / n;
+    for (size_t i = 0; i < n; ++i)
+    {
+        err = err + w * (a[i] - b[i]) * (a[i] - b[i]);
+    }
+
+    return err;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -26,18 +48,16 @@ int main(int argc, const char *argv[])
         return -1;
     }
 
+    float buf[65536] = {0};
+    float buf1[65536] = {.0};
     struct vkllm_tensor *tensor;
-    uint32_t shapes[] = {1, 1, 1, 256};
+    uint32_t shapes[] = {1, 1, 1, 65536};
     vkllm_tensor_new(context, device, "t0", shapes, vkllm_float32, VKLLM_OP_ADD, NULL, 0, NULL, 0, false, &tensor);
 
-    float buf[256] = {0};
-    for (int i = 0; i < 256; ++i)
-    {
-        buf[i] = (float)i;
-    }
+    random_buf(buf, 65536);
 
-    float buf1[256] = {0};
-
+    float mse = float_mse(buf, buf1, 65536);
+    log_info("mse before transfer: %f", mse);
     vkllm_commands_begin(context, commands);
     vkllm_commands_upload(context, commands, tensor, (const uint8_t *)buf, sizeof(buf));
     vkllm_commands_download(context, commands, tensor, (uint8_t *)buf1, sizeof(buf1));
@@ -45,11 +65,12 @@ int main(int argc, const char *argv[])
     vkllm_commands_submit(context, commands);
     vkllm_commands_wait_exec(context, commands);
 
-    fprintf(stderr, "download output: \n");
-    for (int i = 0; i < 256; ++i)
-    {
-        fprintf(stderr, "%f ", buf1[i]);
-    }
-    fprintf(stderr, "\n");
+    mse = float_mse(buf, buf1, 65536);
+    log_info("mse after transfer: %f", mse);
+
+    vkllm_tensor_free(context, tensor);
+    vkllm_commands_free(context, commands);
+    vkllm_gpu_device_free(context, device);
+    vkllm_context_free(context);
     return 0;
 }
