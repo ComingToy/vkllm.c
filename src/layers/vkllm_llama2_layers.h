4e62986d73d193f09e0206476cf9284222f50a79
KK <conleylee@foxmail.com>
2026-02-17 14:27:56 +0800

    ✨ feat(llama2): Add Llama2 transformer block builder
    - Added struct `vkllm_llama2_transformer_block_params` to encapsulate attention and FFN parameters.
    - Implemented `vkllm_llama2_build_transformer_block` function to construct the transformer block layers.

--- src/layers/vkllm_llama2_layers.h
+++ src/layers/vkllm_llama2_layers.h
@@ -27,6 +27,12 @@ struct vkllm_llama2_ffn_layer_params
     float norm_eps;
 };
 
+struct vkllm_llama2_transformer_block_params
+{
+    struct vkllm_llama2_ffn_layer_params ffn;
+    struct vkllm_llama2_self_attn_layer_params attn;
+};
+
 extern vkllm_err_t vkllm_llama2_build_self_attn_layer(struct vkllm_context *context, struct vkllm_graph *graph,
                                                       struct vkllm_tensor *input,
                                                       struct vkllm_llama2_self_attn_layer_params params);
@@ -34,4 +40,8 @@ extern vkllm_err_t vkllm_llama2_build_self_attn_layer(struct vkllm_context *cont
 extern vkllm_err_t vkllm_llama2_build_ffn_layer(struct vkllm_context *context, struct vkllm_graph *graph,
                                                 struct vkllm_tensor *input,
                                                 struct vkllm_llama2_ffn_layer_params params);
+
+extern vkllm_err_t vkllm_llama2_build_transformer_block(struct vkllm_context *context, struct vkllm_graph *graph,
+                                                        struct vkllm_tensor *input,
+                                                        struct vkllm_llama2_transformer_block_params params);
 #endif
